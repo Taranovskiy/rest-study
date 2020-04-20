958dda33cb61998ea9adacfea4cac7571ddc6d96
Vladimir Taranovskiy <8756136@gmail.com>
2020-04-20 22:14:20 +0300

    Fetch initial data implemented

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const CONTACTS = [
+  {
+    id: 1,
+    name: 'Jonh',
+    value: '+7-900-100-10-10',
+    marked: false,
+  }
+];
 
+app.get('/api/contacts', (req, res) => {
+  res.status(200).json(CONTACTS);
+})
 
 app.use(express.static(path.resolve(__dirname, 'client')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/index.html'));
 });
--- client/frontend.js
+++ client/frontend.js
@@ -1,5 +1,15 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js';
 
+Vue.component('loader', {
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center;">
+      <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</>
+      </div>
+    </div>
+  `,
+})
+
 new Vue({
   el: '#app',
   data: () => ({
@@ -7,12 +17,18 @@ new Vue({
       name: '',
       value: '',
     },
-    contacts: []
+    contacts: [],
+    loading: false,
   }),
   computed: {
     canCreateCard() {
       return this.form.name.trim() && this.form.value.trim();
     },
+  },
+  async mounted() {
+    this.loading = true;
+    this.contacts = await request('/api/contacts');
+    this.loading = false;
   },
   methods: {
     createContact() {
@@ -28,4 +44,25 @@ new Vue({
       this.contacts = this.contacts.filter(el => el.id !== id);
     }
   },
-})
+});
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {};
+    let body;
+
+    if (data) {
+      headers['Content-Type'] = 'application/json';
+      body = JSON.stringify(data);
+    }
+
+    const response = await fetch(url, {
+      method,
+      headers,
+      body,
+    });
+    return await response.json();
+  } catch (error) {
+    console.warn('Error: ', error.message);
+  }
+}
